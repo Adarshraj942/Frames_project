9efb75e449a5eac9c01122827e8588fb70f974ae
Adarsh Raj <ahazadarsh0014@gmail.com>
2022-05-04 21:47:01 +0530

    second commit

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=(done)=>{
-    const url='mongodb://localhost:27017'
+    const url=process.env.ATLAS
     const dbname='shopping'
 
     mongoCLlient.connect(url,(err,data)=>{
